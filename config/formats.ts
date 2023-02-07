54a702eea56720d2f9bcf79e3cdf1487fcbb9299
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-02-07 11:36:53 +0000

    stupidest fix

--- config/formats.ts
+++ config/formats.ts
@@ -25,7 +25,7 @@ export const Formats: FormatList = [
 		section: "Insurgence Draft",
 	},
 	{
-		name: "HBF",
+		name: "[Gen 9] HBF",
 		desc: `Includes all HBF changes and standard ruleset.`,
 		mod: 'insurgencehbf',
 		ruleset: ['Standard'],
