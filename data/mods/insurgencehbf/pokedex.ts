623033e549dfb1e67fbbc91af7bab2d4fbbe3db3
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-02-07 15:17:39 +0000

    Revert "delete dex and rulesets"
    This reverts commit fe2dff47b876273ddbd571ac4025eba5d2535235.

---
+++ data/mods/insurgencehbf/pokedex.ts
@@ -0,0 +1,9 @@
+export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
+	bisharpmega: {
+		inherit: true,
+	},
+    bulbasaurdelta: {
+		inherit: true,
+        abilities: {0: "Psycho Call", H: "Regenerator"},
+	},
+};
