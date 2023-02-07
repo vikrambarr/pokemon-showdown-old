fe2dff47b876273ddbd571ac4025eba5d2535235
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-02-07 11:34:33 +0000

    delete dex and rulesets

--- data/mods/insurgencehbf/pokedex.ts
+++
@@ -1,9 +0,0 @@
-export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
-	bisharpmega: {
-		inherit: true,
-	},
-    bulbasaurdelta: {
-		inherit: true,
-        abilities: {0: "Psycho Call", H: "Regenerator"},
-	},
-};
