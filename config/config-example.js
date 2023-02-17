c36626c6304d5ad68c5652b459f95c1f75d679c3
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-02-17 12:41:22 +0000

    Revert "prepare to revert"
    This reverts commit b8135510c4a3aee54f8995965bcc3232275fc96e.

--- config/config-example.js
+++ config/config-example.js
@@ -177,7 +177,7 @@ Y929lRybWEiKUr+4Yw2O1W0CAwEAAQ==
  */
 exports.routes = {
 	root: 'pokemonshowdown.com',
-	client: 'localhost',
+	client: 'play.pokemonshowdown.com',
 	dex: 'dex.pokemonshowdown.com',
 	replays: 'replay.pokemonshowdown.com',
 };
