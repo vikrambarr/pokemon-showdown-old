ceb5cc28eb344c820525db3d361e5e2723517334
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-02-18 14:06:53 +0000

    reduce grooviness

--- data/moves.ts
+++ data/moves.ts
@@ -22014,9 +22014,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		},
 		onPrepareHit(target, source) {
 			this.add('-anim', source, 'Rain Dance', target);
-			this.add('-message', `${source.name} got it's groove on!`);
-			this.add('-anim', source, 'Rain Dance', target);
-			this.add('-anim', source, 'Rain Dance', target);
+			this.add('-message', `${source.name} got its groove on!`);
 			this.add('-anim', source, 'Rain Dance', target);
 		},
 		weather: 'NewMoon',
