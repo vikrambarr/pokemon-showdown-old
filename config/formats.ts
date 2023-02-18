689b29a97d6c3618b70c4ae09168b678e8be5aaf
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-02-18 00:42:24 +0000

    add custom game back

--- config/formats.ts
+++ config/formats.ts
@@ -88,6 +88,16 @@ export const Formats: FormatList = [
 		mod: 'gen9',
 		ruleset: ['Min Source Gen = 9', 'Obtainable', 'Team Preview', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause'],
 	},
+	{
+		name: "[Gen 9] Custom Game",
+
+		mod: 'gen9',
+		searchShow: false,
+		debug: true,
+		battle: {trunc: Math.trunc},
+		// no restrictions, for serious (other than team preview)
+		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100'],
+	},
 
 	// S/V Doubles
 	///////////////////////////////////////////////////////////////////
