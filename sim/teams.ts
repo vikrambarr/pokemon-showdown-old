32dbe6d4bee904986690ebd287e2d6176c986027
Dunscy2 <87420792+Dunscy@users.noreply.github.com>
2023-02-21 17:56:04 +0000

    level fix attempt pt. 23

--- sim/teams.ts
+++ sim/teams.ts
@@ -108,6 +108,9 @@ export interface PokemonSet {
 	 * Tera Type
 	 */
 	teraType?: string;
+	/**
+	 * Custom Move type
+	 */
 	customMovetype?: string;
 }
 
@@ -180,7 +183,7 @@ export const Teams = new class Teams {
 			}
 
 			// level
-			if (set.level && set.level !== 100) {
+			if (set.level && set.level !== 120) {
 				buf += '|' + set.level;
 			} else {
 				buf += '|';
@@ -194,12 +197,13 @@ export const Teams = new class Teams {
 			}
 
 			if (set.pokeball || set.hpType || set.gigantamax ||
-				(set.dynamaxLevel !== undefined && set.dynamaxLevel !== 10) || set.teraType) {
+				(set.dynamaxLevel !== undefined && set.dynamaxLevel !== 10) || set.teraType || set.customMovetype) {
 				buf += ',' + (set.hpType || '');
 				buf += ',' + this.packName(set.pokeball || '');
 				buf += ',' + (set.gigantamax ? 'G' : '');
 				buf += ',' + (set.dynamaxLevel !== undefined && set.dynamaxLevel !== 10 ? set.dynamaxLevel : '');
 				buf += ',' + (set.teraType || '');
+				buf += ',' + (set.customMovetype || '');
 			}
 		}
 
@@ -331,6 +335,7 @@ export const Teams = new class Teams {
 				set.gigantamax = !!misc[3];
 				set.dynamaxLevel = (misc[4] ? Number(misc[4]) : 10);
 				set.teraType = misc[5];
+				set.customMovetype = misc[6];
 			}
 			if (j < 0) break;
 			i = j + 1;
